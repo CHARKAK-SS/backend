9a4d14d17ad977e64c6aec587ed841ac12c90e61
ParkSieun <110987950+Elecstealer@users.noreply.github.com>
2025-06-13 12:20:12 +0900

    Update TagForPostRepository.java

--- src/main/java/charkak/demo/repository/TagForPostRepository.java
+++ src/main/java/charkak/demo/repository/TagForPostRepository.java
@@ -4,5 +4,4 @@ import charkak.demo.model.TagForPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TagForPostRepository extends JpaRepository<TagForPost, Long> {
-    // TagForPost 엔티티에 대한 CRUD를 JpaRepository가 제공
 }
