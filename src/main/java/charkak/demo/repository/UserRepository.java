e52ae9ce4309f63c414f4bb36389acd6728f37d7
ParkSieun <110987950+Elecstealer@users.noreply.github.com>
2025-06-13 12:20:25 +0900

    Update UserRepository.java

--- src/main/java/charkak/demo/repository/UserRepository.java
+++ src/main/java/charkak/demo/repository/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username); // 로그인 시 사용
-    boolean existsByUsername(String username); // 중복 확인
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
 }
