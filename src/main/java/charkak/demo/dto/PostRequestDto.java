ed6f45c688331ef1fd44a3efc477dbf7672a8f98
ParkSieun <110987950+Elecstealer@users.noreply.github.com>
2025-06-13 12:24:39 +0900

    Update PostRequestDto.java

--- src/main/java/charkak/demo/dto/PostRequestDto.java
+++ src/main/java/charkak/demo/dto/PostRequestDto.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public class PostRequestDto {
     private String placeName;
-    private String dateTime; // "2025-05-24T17:28:00"
+    private String dateTime;
     private String camera;
     private String lens;
     private String aperture;
@@ -15,9 +15,8 @@ public class PostRequestDto {
     private String imageUrl;
     private String thumbnailUrl;
     private String text;
-    private Long userId;  // 유저 ID
+    private Long userId;
 
-    // 태그명
     private String ratingTagName;
     private String countryTagName;
     private String cityTagName;
