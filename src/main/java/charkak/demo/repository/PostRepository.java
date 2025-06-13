8d7d138e8904b67a7adca688fbfadfedbdeb070d
ParkSieun <110987950+Elecstealer@users.noreply.github.com>
2025-06-13 12:19:51 +0900

    Update PostRepository.java

--- src/main/java/charkak/demo/repository/PostRepository.java
+++ src/main/java/charkak/demo/repository/PostRepository.java
@@ -4,5 +4,4 @@ import charkak.demo.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    // Post 엔티티에 대한 CRUD를 JpaRepository가 제공
 }
