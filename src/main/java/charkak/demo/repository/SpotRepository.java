92a9fefe86aebec25cd7c90ad7853d2348715e53
ParkSieun <110987950+Elecstealer@users.noreply.github.com>
2025-06-13 12:20:03 +0900

    Update SpotRepository.java

--- src/main/java/charkak/demo/repository/SpotRepository.java
+++ src/main/java/charkak/demo/repository/SpotRepository.java
@@ -10,11 +10,9 @@ import java.util.List;
 public interface SpotRepository extends JpaRepository<Spot, Long> {
     List<Spot> findByNameContaining(String keyword);
 
-    // 등록 중복 확인
     @Query("SELECT s FROM Spot s WHERE REPLACE(s.name, ' ', '') = :name")
     List<Spot> findByNameIgnoreSpaces(@Param("name") String name);
 
-    // 검색 공백 확인
     @Query("SELECT s FROM Spot s WHERE REPLACE(s.name, ' ', '') LIKE %:sanitized%")
     List<Spot> searchBySanitizedName(@Param("sanitized") String sanitized);
 }
